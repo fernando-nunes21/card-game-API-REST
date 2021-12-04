f024d7f17a5e7c22bcc45e31b268e89957709182
fernando-nunes21 <nmgamesforfun@gmail.com>
2021-12-04 17:13:26 -0300

    Add base project
    Adicionando start projeto groovy + spring

---
+++ src/main/groovy/com/project/cardgame/CardGameApplication.groovy
@@ -0,0 +1,13 @@
+package com.project.cardgame
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class CardGameApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(CardGameApplication, args)
+	}
+
+}
